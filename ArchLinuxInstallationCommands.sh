80051ac50e9645cde52cf379f2dd21f05bac602b
Daniel Humberto Reyes Rocha <DanielHumbertoReyesRocha@outlook.com>
2025-01-30 14:02:58 -0600

    Create  ArchLinuxInstallationCommands.sh

---
+++ ArchLinuxInstallationCommands.sh
@@ -0,0 +1,59 @@
+#  Instalar Arch
+archinstall
+
+
+#  Contraseña Arch
+arch  arch  
+
+#  Perfil y Contraseña Arch
+arch  arch  arch
+
+#  Paquetes Adicionales 
+firefox plasma-meta konsole neofetch
+
+
+#  chroot en la Instalación
+NO
+
+#  Reinicio de Arch
+reboot
+
+
+
+
+
+#  Perfil y Contraseña Arch  
+arch  arch
+
+
+#  Actualizar Arch
+sudo pacman -Syu
+
+#  Instalación de Xorg
+sudo pacman -S xorg-server xorg-xinit
+
+#  Entorno de escritorio KDE Plasma
+sudo pacman -S plasma-desktop
+
+#  Gestor de Pantalla LightDM
+sudo pacman -S sddm 
+
+#  Gestor de Pantalla LightDM
+sudo systemctl enable sddm
+
+#  Gestor de Pantalla LightDM
+sudo systemctl start sddm
+
+
+#  KDE Plasma (x11)
+Plasma (x11)
+
+#  Contraseña Arch  
+arch  arch
+
+
+
+
+
+# Neofetch en Terminal
+neofetch 
